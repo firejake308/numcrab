b5d893ec0f0340fda5ae06469e2cb09d018ecf50
Adel <sean.dashals@gmail.com>
2020-11-30 22:37:49 -0600

    Pretty prints flattened array

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ mod utils;
 
 use wasm_bindgen::prelude::*;
 use std::fmt;
+use std::mem;
+use std::convert::TryInto;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -19,6 +21,11 @@ pub fn greet() {
     unsafe {
         alert("Hello, numcrab!");
     }
+}
+
+#[wasm_bindgen]
+pub fn enable_logging() {
+    utils::set_panic_hook();
 }
 
 #[wasm_bindgen]
@@ -82,14 +89,18 @@ pub struct NdArray {
 
 #[wasm_bindgen]
 impl NdArray {
-    pub fn new(shape: Vec<u32>, dtype: Option<Dtype>, buffer: Option<Vec<u32>>) -> NdArray {
+    pub fn new(shape: Vec<u32>, dtype: Option<Dtype>, buffer: Option<Vec<u8>>) -> NdArray {
         let num_els: u32 = shape.iter().product();
         let dtype = match dtype {
             Some(dtype) => dtype,
             None => Dtype::new(Generic::Float64, None, None),
         };
+        let data = match buffer {
+            Some(v) => v,
+            None => Vec::<u8>::with_capacity((num_els * dtype.item_size) as usize)
+        };
         NdArray {
-            data: Vec::with_capacity((num_els * dtype.item_size) as usize),
+            data,
             dtype,
             shape,
         }
@@ -101,6 +112,29 @@ impl NdArray {
 
     pub fn shape(&self) -> String {
         format!("{:?}", self.shape)
+    }
+
+    pub fn pretty_print(&self) -> String {
+        let mut res = String::from("[");
+
+        for i in 0..self.data.len() {
+            if i % self.dtype.item_size as usize == 0 {
+                // add comma in between values (except the first)
+                if i != 0 {
+                    res.push_str(", ");
+                }
+
+                match self.dtype {
+                    Dtype {kind: 'i', alignment: _, item_size: 4} =>
+                        res.push_str(&i32::from_le_bytes(self.data[i..i + 4].try_into().expect("aarggg line 122")).to_string()),
+                    Dtype {kind: 'f', alignment: _, item_size: 8} =>
+                        res.push_str(&f64::from_le_bytes(self.data[i..i + 8].try_into().expect("very tired")).to_string()),
+                    _ => panic!("Not implemented yet")
+                }
+            }
+        }
+        res.push_str("]");
+        res
     }
 }
 
@@ -117,7 +151,7 @@ pub fn get_shape(val: &JsValue) -> Vec<u32> {
 
 #[wasm_bindgen]
 pub fn array(array: js_sys::Array, dtype: Option<Dtype>) -> NdArray {
-    let buffer: Vec<u32> = vec![];
+    let mut buffer: Vec<u8> = vec![];
     // determine dtype
     let mut non_int_found = false;
     let flattened_array = array.flat(MAX_DIM);
@@ -135,6 +169,25 @@ pub fn array(array: js_sys::Array, dtype: Option<Dtype>) -> NdArray {
         Some(dtype) => dtype,
         None => if non_int_found {Dtype::new(Generic::Float64, None, None)} else {Dtype::new(Generic::Int32, None, None) },
     };
+
+    // fill buffer
+    flattened_array.for_each(
+        &mut |val: JsValue, idx: u32, _arr: js_sys::Array| {
+            match dtype {
+                Dtype {kind: 'i', alignment, item_size: 4} => {
+                    let mut bytes = [0u8; mem::size_of::<i32>()];
+                    let converted: i32 = val.as_f64().unwrap() as i32;
+                    buffer.extend(converted.to_le_bytes().iter());
+                },
+                Dtype {kind: 'f', alignment, item_size: 8} => {
+                    let mut bytes = [0u8; mem::size_of::<f64>()];
+                    let converted = val.as_f64().unwrap();
+                    buffer.extend(converted.to_le_bytes().iter());
+                },
+                _ => panic!("Non-integer types have not yet been implemented")
+            };
+        }
+    );
 
     NdArray::new(get_shape(&array), Some(dtype), Some(buffer))
 }
